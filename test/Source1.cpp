847f5aafd5eab817c1588a5db17d6b70da8fd447
leo <leo-delaunay@hotmail.fr>
2017-12-08 16:07:52 +0100

    WIP

--- test/Source1.cpp
+++ test/Source1.cpp
@@ -2,6 +2,7 @@
 #include <opencv2\opencv.hpp>
 #include <stdlib.h>
 #include <math.h>
+#include <numeric>
 using namespace std;
 using namespace cv;
 
@@ -16,8 +17,72 @@ vector<Vec3f> FV;
 
 void filtering(Mat & image, Mat & filteredImage) {
 
-	bilateralFilter(image, filteredImage, 5, 150, 150);
+	Mat tmp;
+	image.copyTo(tmp);
+	for (int i = 0; i < 5; i++) {
+		bilateralFilter(tmp, filteredImage, -1, 2, 2);
+		filteredImage.copyTo(tmp);
+
+		//cvtColor(imageLocalFloat, m_imageInLab, CV_BGR2Lab);
+	}
+	
 }
+
+vector<Point> tirage(const Mat& picture, vector<float>& prob_x, vector< vector<float> >& prob_y, const int& height, const int& width, int numberPoint) 
+{
+
+	Mat src;
+
+	src = picture.clone();
+
+	int count = 0;
+	vector<int> count_y(prob_x.size());
+
+	// We count pixels on the edges
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+
+				prob_x[i] = prob_x[i] + 1;
+				prob_y[i][j] = 1;
+				count = count + 1;
+				count_y[i] = count_y[i] + 1;
+		
+		}
+	}
+
+	// We divided by the count to create a probability
+	std::transform(prob_x.begin(), prob_x.end(), prob_x.begin(), std::bind2nd(std::divides<float>(), count));
+
+	for (unsigned int i = 0; i < count_y.size(); i++)
+	{
+		if (count_y[i] != 0)
+		{
+			std::transform(prob_y[i].begin(), prob_y[i].end(), prob_y[i].begin(), std::bind2nd(std::divides<float>(), count_y[i]));
+		}
+	}
+
+	// Cumulative sum
+	partial_sum(prob_x.begin(), prob_x.end(), prob_x.begin(), plus<double>());
+	for (unsigned int i = 0; i < count_y.size(); i++)
+	{
+		partial_sum(prob_y[i].begin(), prob_y[i].end(), prob_y[i].begin(), plus<double>());
+	}
+
+	vector<Point> result;
+	for (int i = 0; i < numberPoint; i++){
+
+		double rand_x = rand() / (double)RAND_MAX;
+		double rand_y = rand() / (double)RAND_MAX;
+
+		int x = (upper_bound(prob_x.begin(), prob_x.end(), rand_x) - prob_x.begin());
+		int y = (upper_bound(prob_y[x].begin(), prob_y[x].end(), rand_y) - prob_y[x].begin());
+		result.push_back(Point(y, x));
+	}
+	return result;
+}
+
 
 void computeOmega(int radius) {
 	for (int i = 0; i < m_image.rows; i++) {
@@ -37,12 +102,13 @@ void computeFV() {
 
 		filtering(m_imageInLab, m_filteredImage);
 
-		namedWindow("Image fiiltrée", WINDOW_AUTOSIZE);
-		imshow("Image filtrée", m_filteredImage);
+		//namedWindow("Image fiiltrée", WINDOW_AUTOSIZE);
+		
 
 		Mat lab[3];
 		split(m_filteredImage, lab);
-
+	
+	//	imshow("Image filtrée", m_imageInLab);
 		for (auto courant = m_omega.begin(); courant != m_omega.end(); courant++)
 		{
 			Vec3f candidat(lab[0].at<float>(*courant), lab[1].at<float>(*courant), lab[2].at<float>(*courant));
@@ -50,13 +116,15 @@ void computeFV() {
 		}
 }
 
-void computeDP() {
+
+//return DP mat (normalize et CV_8U)
+Mat computeDP() {
 
 		float maxValue = 0;
 		Mat dp(m_image.rows, m_image.cols, CV_32FC1);
 	
 		Mat lab[3];
-		split(m_imageInLab, lab);
+		split(m_filteredImage, lab);
 		for (int i = 0; i < m_image.rows; i++) {
 			for (int j = 0; j < m_image.cols; j++) {
 	
@@ -88,7 +156,43 @@ void computeDP() {
 		cv::normalize(dp, todisplay, 0, 255, NORM_MINMAX,CV_8U);
 	//	todisplay = norm_0_255(dp);
 		namedWindow("hop", WINDOW_AUTOSIZE);// Create a window for display.
-		imshow("hop",todisplay);
+		imshow("hop", todisplay);
+		return todisplay;
+}
+
+void findPointsToWheather(Mat & wheatheringMap, int nombre) {
+	vector<float> prob_x(wheatheringMap.rows);
+	vector<vector<float>> prob_y(wheatheringMap.rows, vector<float>(wheatheringMap.cols));
+	vector<Point> pointsToWheather = tirage(wheatheringMap, prob_x, prob_y, wheatheringMap.rows, wheatheringMap.cols, 15);
+	/*
+	Mat todisplay;
+	wheatheringMap.convertTo(todisplay, CV_8UC3);
+	for (int i = 0; i < pointsToWheather.size(); i++) {
+		cout << pointsToWheather[i].x << " " << pointsToWheather[i].y << endl;
+		todisplay.at<Vec3i>(pointsToWheather[i].x, pointsToWheather[i].y)[0] = 255;
+	}
+
+	namedWindow("hophop", WINDOW_AUTOSIZE);// Create a window for display.
+	imshow("hophop", todisplay);
+	*/
+}
+
+void weatherPoints(const std::vector<cv::Point> pointsToWeather, const cv::Mat& patch)
+{
+	cv::Mat map;
+	cv::Mat weatheredImage(m_image.rows, m_image.cols, CV_32FC3);
+	for (int p = 0; p < pointsToWeather.size(); p++)
+	{
+		cv::Point currentPoint = pointsToWeather[p];
+		for (int i_patch = std::floor(currentPoint.x - patch.rows / 2); i_patch < std::floor(currentPoint.x + patch.rows / 2); i_patch++)
+		{
+			for (int j_patch = std::floor(currentPoint.y - patch.cols / 2); j_patch < std::floor(currentPoint.y + patch.cols / 2); j_patch++)
+			{
+				weatheredImage.at<Vec3f>(i_patch, j_patch)[0] = (1 - map.at<float>(currentPoint)) * m_image.at<Vec3f>(i_patch, j_patch)
+					+ map.at<float>(currentPoint) * patch.at<Vec3f>(i_patch, j_patch);
+			}
+		}
+	}
 }
 
 void on_mouse(int e, int x, int y, int d, void *ptr)
@@ -101,10 +205,12 @@ void on_mouse(int e, int x, int y, int d, void *ptr)
 		cout << "Lancement des calculs" << endl;
 		computeOmega(10);
 		computeFV();
-		computeDP();
-
+		Mat dp = computeDP();
+		findPointsToWheather(dp, 15);
+		
 	}
 }
+
 
 
 
